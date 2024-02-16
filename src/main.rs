be37a84176b530b20368370c48f008459298f1d0
Vadim <vadimkl2004@mail.ru>
2024-02-16 20:14:21 +0300

    add delay at the end

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use r3dgd::run;
+use text_io::read;
 
 fn main() {
     match run() {
         Ok(_) => {println!("SUCCESS!")},
         Err(e) => {println!("ERROR: {e}")}
     }
-
+    println!("Click \"Enter\" to finish");
+    let _: String = read!("{}\n");
 }
